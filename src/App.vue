2f6a40245dd7fc806168515c6237052da9c42ee3
gyyxtx <1042766239@qq.com>
2019-07-29 14:56:33 +0800

    初始化+rem适配

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <van-button type="primary">主要按钮</van-button>
+    <div class="a"></div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+.a{
+  width:100px;
+  height: 100px;
+  background: red;
+}
+</style>
